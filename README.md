707b853e69651d05519477d90d448f179249ec79
yosup <yosup1004@gmail.com>
2020-07-02 16:20:45 +0900

    html add

--- README.md
+++
@@ -1,2 +0,0 @@
-# HTMLAndCSSProject
-HTML 과 CSS 만 활용한 반응형 웹사이트
---
+++ project8/gulpfile.babel.js
@@ -0,0 +1,27 @@
+import gulp from "gulp";
+import sass from "gulp-sass";
+
+sass.compiler = require("node-sass");
+
+const routes = {
+  scss: {
+    watch: "src/scss/*.scss",
+    src: "src/scss/style.scss",
+    dest: "dist",
+  },
+};
+
+const watch = () => {
+  gulp.watch(routes.scss.watch, style);
+};
+
+const style = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
+
+const assets = gulp.series([style]);
+const live = gulp.series([watch]);
+
+export const dev = gulp.series([assets, live]);
